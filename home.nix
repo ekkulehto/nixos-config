5c020d3e340dd7b75fcb9db736504b2ca261f139
Ekku Lehto <ekkulehto@proton.me>
2026-01-26 02:43:15 +0200

    feat: Add shell alias for building desktop enviroment

--- home.nix
+++ home.nix
@@ -19,6 +19,9 @@
 
   programs.bash = {
     enable = true;
+    shellAliases = {
+      "build-desktop" = "sudo nixos-rebuild switch --flake /etc/nixos/#desktop";
+    }
     profileExtra = ''
       if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
         exec hyprland
