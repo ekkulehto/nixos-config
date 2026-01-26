2335074ba9fecac73c965c2523b28f083ec94edd
Ekku Lehto <ekkulehto@proton.me>
2026-01-26 02:45:28 +0200

    fix: correct typo

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
     enable = true;
     shellAliases = {
       "build-desktop" = "sudo nixos-rebuild switch --flake /etc/nixos/#desktop";
-    }
+    };
     profileExtra = ''
       if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
         exec hyprland
