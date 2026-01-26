9a78708bb3a46d257b37cbeb5ec5e94d04165e0b
Ekku Lehto <ekkulehto@proton.me>
2026-01-26 21:34:10 +0200

    feat: Add neovim config to home manager

--- home.nix
+++ home.nix
@@ -28,4 +28,24 @@
       fi
     '';
   };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    
+    extraLuaConfig = ''
+      vim.opt.expandtab = true
+      vim.opt.tabstop = 2
+      vim.opt.shiftwidth = 2
+      vim.opt.smartindent = true
+      vim.opt.autoindent = true
+      vim.opt.clipboard = "unnamedplus"
+    '';
+  };
+
+  home.packages = with pkgs; [
+    wl-clipboard
+  ];
 }
