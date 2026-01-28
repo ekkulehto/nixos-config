ea1d9db21d20443a5fa4d98446a7d3114e386fef
Ekku Lehto <ekkulehto@proton.me>
2026-01-28 18:37:52 +0200

    refactor: Move NixOS core modules into core folder

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -5,17 +5,9 @@
     [
       ./hardware-configuration.nix
 
-      ../../modules/nixos/boot.nix
-      ../../modules/nixos/packages.nix
-      ../../modules/nixos/users/ekku.nix
-      ../../modules/nixos/environment-variables.nix
+      ../../modules/nixos/core
       ../../modules/nixos/programs/hyprland.nix
       ../../modules/nixos/programs/firefox.nix
-      ../../modules/nixos/locale.nix
-      ../../modules/nixos/hardware.nix
-      ../../modules/nixos/networking.nix
-      ../../modules/nixos/login.nix
-      ../../modules/nixos/nix-settings.nix
     ];
 
   system.stateVersion = "25.11";
---
+++ modules/nixos/core/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    ./boot.nix
+    ./env.nix
+    ./environment-variables.nix
+    ./hardware.nix
+    ./locale.nix
+    ./login.nix
+    ./networking.nix
+    ./nix-settings.nix
+    ./packages.nix
+  ]
+}
