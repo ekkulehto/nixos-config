74934152731ffb0def34666918cffc49f01415bb
Ekku Lehto <ekkulehto@proton.me>
2026-01-28 16:33:19 +0200

    refactor: Move packages into packages.nix

--- home.nix
+++ home.nix
@@ -5,19 +5,11 @@
     ./modules/home-manager/programs/git.nix
     ./modules/home-manager/programs/bash.nix
     ./modules/home-manager/programs/neovim.nix
+    ./modules/home-manager/packages.nix
   ];
-
 
   home = {
     username = "ekku";
     stateVersion = "25.11";
   };
-
-
-
-
-  home.packages = with pkgs; [
-    wl-clipboard
-    ripgrep
-  ];
 }
