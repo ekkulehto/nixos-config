864339143b1f1e97606fe08a8b8fd3ba9530e264
Ekku Lehto <ekkulehto@proton.me>
2026-02-03 00:45:07 +0200

    feat: Enable firewall and moonlight ports

--- modules/nixos/core/networking.nix
+++ modules/nixos/core/networking.nix
@@ -5,4 +5,11 @@
     hostName = "nixos";
     networkmanager.enable = true;
   };
+
+  networking.firewall = {
+    enable = true;
+
+  allowedTCPPorts = [ 47984 47989 47990 48010 ];
+  allowedUDPPorts = [ 5353 47998 47999 48000 48002 48010 ];
+  };
 }
