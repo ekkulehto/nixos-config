f59a65e5468aa42a3aef9ac3f8d6b73da710d752
Ekku Lehto <ekkulehto@proton.me>
2026-02-04 15:43:49 +0200

    fix: Try to use graphical session instead for moonlight autostart on boot

--- homes/ekku/services/moonlight-autostream.nix
+++ homes/ekku/services/moonlight-autostream.nix
@@ -9,33 +9,31 @@ in
 
   systemd.user.services.moonlight-autostream = {
     Unit = {
-      Description = "Moonlight Autostream (X11 Mode)";
-      # Käytetään hyprland-session.targetia, se on NixOS:ssa luotettavampi
-      After = [ "hyprland-session.target" "network-online.target" ];
+      Description = "Moonlight Autostream";
+      After = [ "graphical-session.target" "network-online.target" ];
       Wants = [ "network-online.target" ];
-      # Jos Hyprland sammuu, sammuta tämäkin
-      PartOf = [ "hyprland-session.target" ];
+      PartOf = [ "graphical-session.target" ];
     };
 
     Service = {
-      # Käynnistyskomento
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+      
       ExecStart = ''
         ${lib.getExe pkgs.moonlight-qt} stream "${host}" "${app}" --quit-after
       '';
 
       Restart = "always";
-      RestartSec = "2s";      
+      RestartSec = "2s";
 
       Environment = [
         "SDL_VIDEODRIVER=x11"
         "QT_QPA_PLATFORM=xcb"
         "QT_AUTO_SCREEN_SCALE_FACTOR=0"
-        "DISPLAY=:0" 
       ];
     };
 
     Install = {
-      WantedBy = [ "hyprland-session.target" ];
+      WantedBy = [ "graphical-session.target" ];
     };
   };
 }
