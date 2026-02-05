8410d19b3af2e0c0eee73d49ded9c757dc66a317
Ekku Lehto <ekkulehto@proton.me>
2026-02-06 01:42:48 +0200

    feat: Add ripdrag to yazi

--- modules/home-manager/programs/yazi/keymap.nix
+++ modules/home-manager/programs/yazi/keymap.nix
@@ -11,6 +11,17 @@
           run = "shell --orphan --confirm kitty";
           desc = "Open kitty here";
         }
+
+        {
+          on = [ "<A-d>" ];
+          run = ''
+            shell -- ripdrag --no-click --and-exit --icon-size 64 --target --all "$@" \
+              | while read -r filepath; do
+                  cp -nR -- "$filepath" .
+                done
+          '';
+          desc = "Drag-n-drop files from/to Yazi (no overwrite)";
+        }
       ];
     };
   };
