3fc15ff55a94aca9dfea9e50aae9d0ba79e3e437
Ekku Lehto <ekkulehto@proton.me>
2026-02-06 17:36:34 +0200

    TEST: Try vibecoded yazi-plugin for seamless drag-and-drop

--- modules/home-manager/programs/yazi/keymap.nix
+++ modules/home-manager/programs/yazi/keymap.nix
@@ -12,15 +12,22 @@
           desc = "Open kitty here";
         }
 
+        # {
+        #   on = [ "<A-d>" ];
+        #   run = ''
+        #     shell -- ripdrag --no-click --and-exit --icon-size 64 --target --all "$@" \
+        #       | while read -r filepath; do
+        #           cp -nR -- "$filepath" .
+        #         done
+        #   '';
+        #   desc = "Drag-n-drop files from/to Yazi (no overwrite)";
+        # }
+
         {
-          on = [ "<A-d>" ];
-          run = ''
-            shell -- ripdrag --no-click --and-exit --icon-size 64 --target --all "$@" \
-              | while read -r filepath; do
-                  cp -nR -- "$filepath" .
-                done
-          '';
-          desc = "Drag-n-drop files from/to Yazi (no overwrite)";
+          [[mgr.prepend_keymap]];
+          on   = [ "<A-d>" ];
+          run  = "plugin hyprdnd-for-yazi";
+          desc = "Hyprland DnD overlay (hyprdnd-for-yazi)";
         }
       ];
     };
