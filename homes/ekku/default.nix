fa6602dff8dcbffdd67aeebba5c570925f3eb76d
Ekku Lehto <ekkulehto@proton.me>
2026-02-18 00:34:16 +0200

    fix: Correct imports

--- homes/ekku/default.nix
+++ homes/ekku/default.nix
@@ -2,7 +2,8 @@
 
 {
   imports = [
-    ../../modules/home-manager/programs
+    ../../modules/home-manager/programs/common
+    ../../modules/home-manager/programs/extra/desktop
     ../../modules/home-manager/packages/common
     ../../modules/home-manager/packages/extra/desktop
 
