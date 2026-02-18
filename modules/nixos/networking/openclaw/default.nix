8a9157ac015700239105abad1720a19f2d4beab8
Ekku Lehto <ekkulehto@proton.me>
2026-02-18 14:20:01 +0200

    feat: Enable ssh and open port 22

--- modules/nixos/networking/openclaw/default.nix
+++ modules/nixos/networking/openclaw/default.nix
@@ -4,5 +4,19 @@
   networking = {
     hostName = "openclaw";
     networkmanager.enable = true;
+    firewall = {
+      allowedTCPPorts = [
+        22
+      ];
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
   };
 }
