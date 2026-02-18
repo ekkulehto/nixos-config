93917ac3e1bdc548a1de6f580458c2ae4fd91ca6
Ekku Lehto <ekkulehto@proton.me>
2026-02-19 00:52:59 +0200

    fix: Try to fix neovim not autocompleting file paths

--- home/common/programs/nvf/default.nix
+++ home/common/programs/nvf/default.nix
@@ -29,12 +29,26 @@
       lineNumberMode = "relNumber";
 
       lsp.enable = true;
-
       languages.nix.enable = true;
 
       autopairs.nvim-autopairs.enable = true;
-      autocomplete.nvim-cmp.enable = true;
+
+      autocomplete.nvim-cmp = {
+        enable = true;
+
+        sources = {
+          "nvim-cmp" = null;
+          "nvim_lsp" = "[LSP]";
+          "path" = "[Path]";
+          "buffer" = "[Buffer]";
+        };
+
+        sourcePlugins = with pkgs.vimPlugins; [
+          cmp-nvim-lsp
+          cmp-path
+          cmp-buffer
+        ];
+      };
     };
   };
 }
-
