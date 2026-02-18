583c1ccd8cc830cc7d3c7eb1408dc7dfa1882f0c
Ekku Lehto <ekkulehto@proton.me>
2026-02-18 20:43:04 +0200

    feat: Rebuild old neovim config in nvf

--- home/common/programs/nvf/default.nix
+++ home/common/programs/nvf/default.nix
@@ -1,4 +1,4 @@
-{ nvf, ... }:
+{ pkgs, nvf, ... }:
 
 {
   imports = [
@@ -7,5 +7,65 @@
 
   programs.nvf = {
     enable = true;
+
+    enableManpages = true;
+
+    settings = {
+      vim = {
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+
+        options = {
+          expandtab = true;
+          tabstop = 2;
+          shiftwidth = 2;
+          smartindent = true;
+          autoindent = true;
+
+          clipboard = "unnamedplus";
+
+          number = true;
+          relativenumber = true;
+        };
+
+        lsp = {
+          enable = true;
+        };
+
+        languages.nix = {
+          lsp.enable = true;
+          lsp.servers = [ "nil" ];
+        };
+
+        autopairs.nvim-autopairs = {
+          enable = true;
+          setupOpts = { };
+        };
+
+        autocomplete.nvim-cmp = {
+          enable = true;
+
+          sourcePlugins = with pkgs.vimPlugins; [
+            cmp-nvim-lsp
+            cmp-path
+            cmp-buffer
+          ];
+
+          sources = {
+            nvim_lsp = "[LSP]";
+            path = "[Path]";
+            buffer = "[Buffer]";
+          };
+
+          mappings = {
+            next = "<Tab>";
+            previous = "<S-Tab>";
+            confirm = "<CR>";
+          };
+        };
+      };
+    };
   };
 }
+
