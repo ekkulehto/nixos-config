f8ba404e642d59958fb3278fb4c94bdee875827b
Ekku Lehto <ekkulehto@proton.me>
2026-02-19 22:41:47 +0200

    fix: Use "stdenv.hostPlatform.system" instead of "system"

--- nixos/common/packages/agenix.nix
+++ nixos/common/packages/agenix.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = [
-    inputs.agenix.packages.${pkgs.system}.default
+    inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
