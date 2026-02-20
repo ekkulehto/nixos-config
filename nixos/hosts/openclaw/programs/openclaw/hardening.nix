0e568168c410f7ede95bd767be4fedb318564f7d
Ekku Lehto <ekkulehto@proton.me>
2026-02-20 18:21:04 +0200

    feat: Add more hardening for openclaw service

--- nixos/hosts/openclaw/programs/openclaw/hardening.nix
+++ nixos/hosts/openclaw/programs/openclaw/hardening.nix
@@ -1,27 +1,60 @@
 { stateDir }:
-
 {
   NoNewPrivileges = true;
-  PrivateTmp = true;
-  PrivateDevices = true;
+  RemoveIPC = true;
 
   ProtectSystem = "strict";
   ProtectHome = true;
+  ProtectProc = "invisible";
+  ProcSubset = "pid";
 
-  ProtectKernelTunables = true;
-  ProtectKernelModules = true;
-  ProtectControlGroups = true;
-  ProtectKernelLogs = true;
-  ProtectClock = true;
-  ProtectHostname = true;
-
-  RestrictSUIDSGID = true;
-  LockPersonality = true;
-  RestrictRealtime = true;
-  RestrictNamespaces = true;
-
-  MemoryDenyWriteExecute = false;
+  PrivateTmp = true;
+  PrivateDevices = true;
+  PrivateUsers = true;
 
   ReadWritePaths = [ stateDir ];
   UMask = "0077";
+
+  ProtectKernelTunables = true;
+  ProtectKernelModules = true;
+  ProtectKernelLogs = true;
+  ProtectControlGroups = true;
+  ProtectClock = true;
+  ProtectHostname = true;
+
+  LockPersonality = true;
+  RestrictRealtime = true;
+  RestrictSUIDSGID = true;
+  RestrictNamespaces = true;
+
+  CapabilityBoundingSet = "";
+  AmbientCapabilities = "";
+
+  MemoryDenyWriteExecute = true;
+
+  SystemCallArchitectures = "native";
+
+  SystemCallFilter = [
+    "~@clock"
+    "~@cpu-emulation"
+    "~@debug"
+    "~@module"
+    "~@mount"
+    "~@obsolete"
+    "~@privileged"
+    "~@raw-io"
+    "~@reboot"
+    "~@resources"
+    "~@swap"
+  ];
+
+  RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" ];
+
+  # IPAddressDeny = [
+  #   "10.0.0.0/8"
+  #   "172.16.0.0/12"
+  #   "192.168.0.0/16"
+  #   "169.254.0.0/16"
+  # ];
 }
+
